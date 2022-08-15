c54c848fdd479ce8b5050c0c3c54d44cc6d2385f
Advanced Develope <advanced.develope@gmail.com>
2022-08-15 23:10:14 +0500

    Add styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
+import "./Button.css";
 
 export interface IButton {
   label: string;
 }
 
 const Button = ({ label }: IButton) => {
-  return <button>{label}</button>;
+  return <button className="un_button">{label}</button>;
 };
 
 export default React.memo(Button)
