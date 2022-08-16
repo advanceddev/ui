7fcea48a5c9c5ca7e69699d474215763828c5a9c
Advanced Develope <advanced.develope@gmail.com>
2022-08-16 21:50:50 +0500

    Remove unused import

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import replace from 'rollup-plugin-replace';
 import typescript from "@rollup/plugin-typescript";
 import babel from 'rollup-plugin-babel';
